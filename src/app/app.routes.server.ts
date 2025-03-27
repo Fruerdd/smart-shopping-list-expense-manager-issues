7003b0189e8a2fa23692cef0f7abb74ec88db2ba
Pavel <103207482+Fruerdd@users.noreply.github.com>
2025-03-27 01:05:34 +0100

    feat(header): Differentiate active menu item style for mobile vs. desktop
    - different layouts for mobile and desktop views
    
    - creation of the header component to use it in the future pages

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,14 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideServerRendering } from '@angular/platform-server';
+import { provideServerRouting, ServerRoute } from '@angular/ssr';
+import { appConfig } from './app.config';
+import { routes } from './app-routing.module';
 
-export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
-];
+const serverConfig: ApplicationConfig = {
+  providers: [
+    provideServerRendering(),
+    provideServerRouting(routes as ServerRoute[]) // ✅ Ensure TypeScript understands this is `ServerRoute[]`
+  ]
+};
+
+export const config = mergeApplicationConfig(appConfig, serverConfig);

a763672a8f22f0c735bf780caddb446df777fa21
rikta5 <tarik.basic@stu.ssst.edu.ba>
2025-03-28 14:39:28 +0100

    Restore necessary files and clean up repository.

--- src/app/app.config.ts
+++
@@ -1,6 +0,0 @@
-import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { routes } from './app-routing.module';
-
-export const appConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding())]
-};
